1530123a3196cdc5f486374b9d4a3c3e6c49042d
Tomasz.Rykala <rykalato@hs001mac50202.home>
2015-08-27 20:44:43 +0100

    Updated gradle plugin to 1.3.0, version to 1.0.3 and refactored sample Activity.

--- sample/src/main/java/com/github/florent37/glidepalette/sample/MainActivity.java
+++ sample/src/main/java/com/github/florent37/glidepalette/sample/MainActivity.java
@@ -1,7 +1,7 @@
 package com.github.florent37.glidepalette.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -9,47 +9,35 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.github.florent37.glidepalette.GlidePalette;
 
-//import com.github.florent37.Glidepalette.GlidePalette;
+public class MainActivity extends AppCompatActivity {
 
-
-public class MainActivity extends ActionBarActivity {
-
-    ImageView imageView;
-
-    TextView textVibrant;
-    TextView textVibrantLight;
-    TextView textVibrantDark;
-    TextView textMuted;
-    TextView textMutedLight;
-    TextView textMutedDark;
+    public static final String URL = "https://www.lepetiterudit.com/wp-content/uploads/2015/04/starry-night.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView) findViewById(R.id.image);
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        TextView textVibrant = (TextView) findViewById(R.id.textVibrant);
+        TextView textVibrantLight = (TextView) findViewById(R.id.textVibrantLight);
+        TextView textVibrantDark = (TextView) findViewById(R.id.textVibrantDark);
+        TextView textMuted = (TextView) findViewById(R.id.textMuted);
+        TextView textMutedLight = (TextView) findViewById(R.id.textMutedLight);
+        TextView textMutedDark = (TextView) findViewById(R.id.textMutedDark);
 
-        textVibrant = (TextView) findViewById(R.id.textVibrant);
-        textVibrantLight = (TextView) findViewById(R.id.textVibrantLight);
-        textVibrantDark = (TextView) findViewById(R.id.textVibrantDark);
-        textMuted = (TextView) findViewById(R.id.textMuted);
-        textMutedLight = (TextView) findViewById(R.id.textMutedLight);
-        textMutedDark = (TextView) findViewById(R.id.textMutedDark);
-
-        String url = "https://www.lepetiterudit.com/wp-content/uploads/2015/04/starry-night.jpg";
-        Glide.with(this).load(url)
+        Glide.with(this).load(URL)
                 .centerCrop()
-                .listener(GlidePalette.with(url)
+                .listener(GlidePalette.with(URL)
                         .use(GlidePalette.Profile.VIBRANT)
-                            .intoBackground(textVibrant, GlidePalette.Swatch.RGB)
-                            .intoTextColor(textVibrant, GlidePalette.Swatch.BODY_TEXT_COLOR)
+                        .intoBackground(textVibrant, GlidePalette.Swatch.RGB)
+                        .intoTextColor(textVibrant, GlidePalette.Swatch.BODY_TEXT_COLOR)
                         .use(GlidePalette.Profile.VIBRANT_DARK)
-                            .intoBackground(textVibrantDark, GlidePalette.Swatch.RGB)
-                            .intoTextColor(textVibrantDark, GlidePalette.Swatch.BODY_TEXT_COLOR)
+                        .intoBackground(textVibrantDark, GlidePalette.Swatch.RGB)
+                        .intoTextColor(textVibrantDark, GlidePalette.Swatch.BODY_TEXT_COLOR)
                         .use(GlidePalette.Profile.VIBRANT_LIGHT)
-                            .intoBackground(textVibrantLight, GlidePalette.Swatch.RGB)
-                            .intoTextColor(textVibrantLight, GlidePalette.Swatch.BODY_TEXT_COLOR)
+                        .intoBackground(textVibrantLight, GlidePalette.Swatch.RGB)
+                        .intoTextColor(textVibrantLight, GlidePalette.Swatch.BODY_TEXT_COLOR)
 
                         .use(GlidePalette.Profile.MUTED)
                         .intoBackground(textMuted, GlidePalette.Swatch.RGB)
@@ -62,6 +50,7 @@ public class MainActivity extends ActionBarActivity {
                         .intoTextColor(textMutedLight, GlidePalette.Swatch.BODY_TEXT_COLOR)
 
                         .intoCallBack(new GlidePalette.CallBack() {
+
                             @Override
                             public void onPaletteLoaded(Palette palette) {
                                 //specific task
@@ -69,5 +58,4 @@ public class MainActivity extends ActionBarActivity {
                         }))
                 .into(imageView);
     }
-
 }
